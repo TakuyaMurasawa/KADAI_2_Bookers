2ebeaa3f48fbf12a1813b4235899040875ddea34
TakuyaMurasawa <bandage104.batt1e37@gmail.com>
2021-06-12 08:31:16 +0000

    [FINISH]KADAI_2

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  # get 'books/index'
+  # get 'books/show'
+  # get 'books/new'
+  # get 'books/edit'
+  # get 'homes/top'
+  root :to => 'homes#top'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :books
 end
